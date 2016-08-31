87d4b50bf1961e123657201bd35cc8172ad7242d
Nikro <gNikro@gmail.com>
2016-09-01 00:55:26 +0300

    +

--- src/slot/net/data/packetModels/BaseSpin.hx
+++ src/slot/net/data/packetModels/BaseSpin.hx
@@ -5,7 +5,7 @@ import slot.net.data.*;
 class BaseSpin extends BasePacketData 
 {	
 	public var type:Int = 1;
-	public var spinData:Int = 3;//double тип спина, 3 - обычный спин, 8 - сделали халф, 7 - сделал ставку и выиграл или проиграл, 4 - нажали коллект, 0 - ласт стейт, 
+	public var spinData:Int = 3;//double тип спина, 3 - обычный спин, 8 - сделали халф, 7 - сделал ставку и выиграл или проиграл, 4 - нажали коллект, 0 - ласт стейт, 5 - выиграли бонус спины, 6 крутим бонус спины, 12 конец бонус спинов
 	public var errorCode:Int = 0;//код ошиюки 1 - not enought credit; 2 - minimum bet is; >= 3 какая то хуерга если 6 или 5 то стоп будет вызван. Так же блочит все кнопки
 	public var userId:Int;
 	public var balance:Int;
@@ -13,12 +13,12 @@ class BaseSpin extends BasePacketData
 	public var reels:ArrayData2;
 	public var indexOfBetPerLine:Int;
 	public var numLines:Int;
-	public var unknown7:Int;
-	public var unknown8:Int;
-	public var unknown9:Int;
-	public var unknown10:Int;
-	public var unknown11:Int;
-	public var unknown12:Int;
+	public var bonusSpinsTotal:Int;
+	public var bonusSpinsLeft:Int;
+	public var bonusSpinWin:Int;//сколько выиграл за текущий спин в бонусных спинах
+	public var bonusGameMultiplayer:Int;
+	public var unknown11:Int;//в бонус спинах оно было 1 хз чт значит	
+	public var bonusSpinWinTotal:Int;//сколько выиграл в бонус спинах за несоклько раз, но не все сюда складывается, возможно именно сам выигрышь бонус спионв сюда не идет а идут только выигрыши именно за бонус спины
 	public var winPerLine:ArrayData2;//выигрышные линии и сколько выиграно, скаттеры последняя линия
 	public var unknown14:Array<Int>;
 	public var gambleHistory:Array<Int>;//история гембла можно слать в виде 0-3 буби, черви, крести, пики
@@ -32,12 +32,12 @@ class BaseSpin extends BasePacketData
 	{	
 		super();	
 		
-		fields = ["type", "spinData", "errorCode", "userId", "balance", "totalWin", "reels", "indexOfBetPerLine", "numLines", "unknown7", "unknown8", "unknown9", "unknown10", "unknown11", "unknown12", "winPerLine", "unknown14", "gambleHistory", "unknown15", "unknown16", "unknown17", "unknown18", "unknown19"];
+		fields = ["type", "spinData", "errorCode", "userId", "balance", "totalWin", "reels", "indexOfBetPerLine", "numLines", "bonusSpinsTotal", "bonusSpinsLeft", "bonusSpinWin", "bonusGameMultiplayer", "unknown11", "bonusSpinWinTotal", "winPerLine", "unknown14", "gambleHistory", "unknown15", "unknown16", "unknown17", "unknown18", "unknown19"];
 		types = ["single", "double", "single", "int", "int", "int", "arrayData2", "double", "double", "int", "int", "int", "int", "int", "int", "arrayData2", "arrayDoubleInt", "array8Double", "notRequiredDouble", "notRequiredArrayIntSingle", "notRequiredArrayIntSingle", "notRequiredArrayIntSingle", "notRequiredArrayIntSingle"];
 	}
 	
 	override public function toString():String
 	{
-		return "[BaseSpin\n type=" + type + "\n spinData=" + spinData + "\n errorCode=" + errorCode + "\n userId=" + userId + "\n balance=" + balance + "\n totalWin=" + totalWin + "\n reels=" + reels + "\n indexOfBetPerLine=" + indexOfBetPerLine + "\n numLines=" + numLines + "\n unknown7=" + unknown7 + "\n unknown8=" + unknown8 + "\n unknown9=" + unknown9 + "\n unknown10=" + unknown10 + "\n unknown11=" + unknown11 + "\n unknown12=" + unknown12 + "\n winPerLine=" + winPerLine + "\n unknown14=" + unknown14 + "\n gambleHistory=" + gambleHistory + "\n unknown15=" + unknown15 + "\n unknown16=" + unknown16 + "\n unknown17=" + unknown17 + "\n unknown18=" + unknown18 + "\n unknown19=" + unknown19 + ")]";
+		return "[BaseSpin\n type=" + type + "\n spinData=" + spinData + "\n errorCode=" + errorCode + "\n userId=" + userId + "\n balance=" + balance + "\n totalWin=" + totalWin + "\n reels=" + reels + "\n indexOfBetPerLine=" + indexOfBetPerLine + "\n numLines=" + numLines + "\n bonusSpinsTotal=" + bonusSpinsTotal + "\n bonusSpinsLeft=" + bonusSpinsLeft + "\n bonusSpinWin=" + bonusSpinWin + "\n bonusGameMultiplayer=" + bonusGameMultiplayer + "\n unknown11=" + unknown11 + "\n bonusSpinWinTotal=" + bonusSpinWinTotal + "\n winPerLine=" + winPerLine + "\n unknown14=" + unknown14 + "\n gambleHistory=" + gambleHistory + "\n unknown15=" + unknown15 + "\n unknown16=" + unknown16 + "\n unknown17=" + unknown17 + "\n unknown18=" + unknown18 + "\n unknown19=" + unknown19 + ")]";
 	}
 }
